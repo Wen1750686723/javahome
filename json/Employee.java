767728fe316c9445d12002e5c9d1bc0188f03d4c
wen1750686723 <wen1750686723@qq.com>
2017-05-20 22:46:57 +0800

    json

---
+++ json/Employee.java
@@ -0,0 +1,25 @@
+package extension.json;
+
+public class Employee {
+    public  String Name;
+    public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public  String Sex;
+    public String getSex() {
+		return Sex;
+	}
+	public void setSex(String sex) {
+		Sex = sex;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public  int Age;
+}
