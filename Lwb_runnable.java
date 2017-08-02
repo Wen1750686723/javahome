4e06323753b9011577e0ee25ac27a3ef3a4f82e0
wen1750686723 <wen1750686723@qq.com>
2017-08-02 09:48:54 +0800

    string

---
+++ Lwb_runnable.java
@@ -0,0 +1,17 @@
+package extension;
+
+public class Lwb_runnable {
+	private volatile boolean active;
+    public void run()
+    {
+        active = true;
+        while (active) // 第一行
+        {
+            // 代码
+        }
+    }
+    public void stop()
+    {
+        active = false; // 第二行
+    }
+}
---
+++ Lwb_runnableTest.java
@@ -0,0 +1,17 @@
+package extension;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Lwb_runnableTest {
+
+	@Test
+	public void test() {
+		String ss="sss";
+		ss="ssss";
+		
+		System.out.println(ss);
+		//fail("Not yet implemented");
+	}
+
+}
